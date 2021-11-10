964feff10af5e12885e9cea71adbd2b4a909b181
Mansi Patel <Mansi Patel@DESKTOP-EKQ4IEO>
2021-11-10 14:06:36 -0500

    I implemented if statements for logic of sides for the triangle to be valid.

--- PolygonChecker/triangleSolver.c
+++ PolygonChecker/triangleSolver.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
 #include <stdbool.h>
-
 #include "triangleSolver.h"
 
 char* analyzeTriangle(int side1, int side2, int side3) {
@@ -22,3 +21,30 @@ char* analyzeTriangle(int side1, int side2, int side3) {
 
 	return result;
 }
+
+char* analyzeTriangle(int side1, int side2, int side3)
+{
+	char* result = "";
+	if (side1 <= 0 || side2 <= 0 || side3 <= 0) 
+	{
+		printf("\n");
+		result = "Not a triangle";
+	}
+	else if (side1 == side2 && side1 == side3)
+	{
+		printf("\n");
+		result = "Equilateral triangle";
+	}
+	else if ((side1 == side2 && side1 != side3) ||
+		(side1 == side3 && side1 != side2))
+	{
+		printf("\n");
+		result = "Isosceles triangle";
+	}
+	else {
+		printf("\n");
+		result = "Scalene triangle";
+	} return result;
+}
+
+
