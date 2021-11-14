68e9c4180b2dbeb504a28c20e629b9d32437b43f
Jainam Yogesh Doshi <Jdoshi3117@conestogac.on.ca>
2021-11-14 16:56:12 -0500

    Implemented, Formulated and prepared the logic and code for calculating the length of the rectangle sides,area and perimeter

--- PolygonChecker/rectangleSolver.c
+++ PolygonChecker/rectangleSolver.c
@@ -11,6 +11,15 @@ void analyzeRectangle(double x1, double y1, double x2, double y2, double x3, dou
 	
 	double distance1, distance2, distance3, distance4, diagonal_distance, area, perimeter;
 
+	distance1 = sqrt(pow(x2 - x1, 2) + pow(y2 - y1, 2)); //A - B
+	distance2 = sqrt(pow(x3 - x2, 2) + pow(y3 - y2, 2)); //B - C
+	distance3 = sqrt(pow(x4 - x3, 2) + pow(y4 - y3, 2)); //C - D
+	distance4 = sqrt(pow(x4 - x1, 2) + pow(y4 - y1, 2)); //D - A
+	diagonal_distance = sqrt(pow(x3 - x1, 2) + pow(y3 - y1, 2)); //A - C
+
+	area = distance1 * distance2;
+	perimeter = (distance1 + distance2) * 2;
+
 	printf("\nDistance between point A and point B is %.2f\n", distance1);
 	printf("\nDistance between point B and point C is %.2f\n", distance2);
 	printf("\nDistance between point C and point D is %.2f\n", distance3);
@@ -18,6 +27,8 @@ void analyzeRectangle(double x1, double y1, double x2, double y2, double x3, dou
 
 	printf("\nLast step to verify that it's a rectangle is to determine whether diagonal divides the rectangle into two right angle triangles");
 	printf("\nDistance between point A and point C(diagonal) is %.2f\n", diagonal_distance);
+
+	
 	
 
 
